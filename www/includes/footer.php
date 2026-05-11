3d9191cc70baf9e07c1bef3d3e3f55736d4e1e03
Sebastian Ignacio Carmona Wright <s.carmonawright@gmail.com>
2026-05-11 00:46:50 -0400

    77

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -2,5 +2,24 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+    // Modo oscuro
+    const toggleButton = document.getElementById('darkModeToggle');
+    const currentMode = localStorage.getItem('darkMode');
+    if (currentMode === 'enabled') {
+        document.body.classList.add('dark-mode');
+        toggleButton.innerHTML = '<i class="bi bi-sun"></i> Modo claro';
+    }
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            toggleButton.innerHTML = '<i class="bi bi-sun"></i> Modo claro';
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+            toggleButton.innerHTML = '<i class="bi bi-moon"></i> Modo oscuro';
+        }
+    });
+</script>
 </body>
 </html>
